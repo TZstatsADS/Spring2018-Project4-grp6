05df96b80fa9884aa354d7a19046b3a24317bc36
Hongyu Li <31630932+Hongyu-Li@users.noreply.github.com>
2018-04-10 18:33:14 -0400

    prediction
    Update

---
+++ lib/prediction.R
@@ -0,0 +1,24 @@
+prediction<-function(df,sim_mat,nbor_list){
+  
+  library(matrixStats)
+  
+  df.mean<-matrix(rep(rowMeans(df,na.rm=T),each=ncol(df)),nrow=nrow(df),byrow = T)
+  df.sd<-matrix(rep(rowSds(df,na.rm=T),each=ncol(df)),nrow=nrow(df),byrow = T)
+  
+  df<-ifelse(is.na(df),0,df)
+  
+  pred<-matrix(NA,ncol=ncol(df),nrow=nrow(df))
+  
+  for (i in 1:nrow(df)){
+    ind<-nbor_list[[i]]
+    nbors_mat<-df[ind,]
+    w<-sim_mat[i,ind]/sum(sim_mat[i,ind])
+    pred.a<- df.mean[i,1] + (w %*% ((nbors_mat-df.mean[ind,])/df.sd[ind,])) * df.sd[i,1]  
+    rm.ind<-which(df[i,]==1)
+    pred.a[rm.ind]<-NA
+    pred[i,]<-pred.a
+  }
+  rownames(pred)<-rownames(df)
+  colnames(pred)<-colnames(df)
+  return(pred)
+}
