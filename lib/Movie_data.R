c6381f6eb06bc0d72dcf671644104dda35af01c0
liupai993 <liupai993@gmail.com>
2018-04-10 20:18:56 -0400

    update

--- lib/Movie_data.R
+++ lib/Movie_data.R
@@ -22,5 +22,18 @@ Transformer <- function(data.set){
 m_train<-Transformer(movie_train)
 m_test<-Transformer(movie_test)
 
+## add the missing columns to test data
+miss_index<-numeric(length(train.index))
+for(i in 1:length(train.index)){
+  miss_index[i]<-train.index[i]%in%test.index
+}
+m_index<-which(miss_index == 0)
+train.index[m_index]
+m_matrix<-matrix(NA, ncol = 22, nrow = 5055)
+colnames(m_matrix)<-m_index
+m_test<-cbind(m_matrix, m_test)
+m_test<-m_test[, order(as.numeric(colnames(m_test)))]
+
+
 write.csv(m_train, "Movie_data_train.csv")
 write.csv(m_test, "Movie_data_test.csv")
